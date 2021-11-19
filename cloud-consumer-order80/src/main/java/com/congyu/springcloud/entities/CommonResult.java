274095e7d69f58b2da9299d63b2fe0e2b1e1aade
wuzs <1696646305@qq.com>
2021-11-19 09:35:22 +0800

    Initial commit

---
+++ cloud-consumer-order80/src/main/java/com/congyu/springcloud/entities/CommonResult.java
@@ -0,0 +1,17 @@
+package com.congyu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CommonResult<T> {
+    private Integer code;
+    private String message;
+    private T       data;
+    public CommonResult(Integer code,String message){
+        this(code,message,null);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/congyu/springcloud/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.congyu.springcloud.controller;
+
+import com.congyu.springcloud.entities.CommonResult;
+import com.congyu.springcloud.entities.Payment;
+import com.congyu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value ="/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result  = paymentService.create(payment);
+        if(result >0){
+            return new CommonResult(200,"插入成功",result);
+        }else{
+            return  new CommonResult(444,"插入失败");
+        }
+
+
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long  id){
+        Payment payment = paymentService.getPaymentById(id);
+        if(payment !=null){
+            return new CommonResult(200,"查询成功",payment);
+        }else{
+            return  new CommonResult(444,"无记录");
+        }
+
+
+    }
+}
